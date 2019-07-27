c97ef9fbfa35e9a8167513f9df5d6e658e7d65db
emaiannone <emaiannone96@gmail.com>
2019-07-27 14:51:18 +0200

    Rearrangement of autoencoder notebook. Metrics are now exported in a CSV file and k-means is looped for 5 different k. New python script files.

--- scripts/clustering.py
+++ scripts/clustering.py
@@ -1,11 +1,9 @@
 from sklearn.cluster import KMeans
-from itertools import groupby
+
 
 def getClustering(features, n_clusters):
-    kmeans = KMeans(n_clusters=n_clusters, random_state=0)
-    res = kmeans.fit_predict(features)
-    kmeans.labels_ +=1
-    res = res -1
-    groups = groupby(sorted(zip(tuple(res), tuple(labelsTest))), lambda x: x[0])
-    counts = {k: dict(Counter(x[1] for x in g)) for k, g in groups}
-    return res, counts
+    kmeans = KMeans(n_clusters=n_clusters, random_state=0, n_init=100)
+    clustering = kmeans.fit_predict(features)
+    kmeans.labels_ += 1
+    clustering = clustering -1
+    return clustering
